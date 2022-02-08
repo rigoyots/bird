2fc7a62ea72ec87129b404db81f79e4dc6ada712
Trentbyu <trentblack@byui.edu>
2022-02-08 14:51:55 -0700

    Create pipe_1.py

---
+++ game/pipe/pipe_1.py
@@ -0,0 +1,16 @@
+
+
+
+
+import raylib
+import pyray
+from pipe.pipe_0 import pipe_0
+
+
+class pipe_1(pipe_0):
+
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+
+
+
