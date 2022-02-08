5d2eb705d41e5f65e1e4c976b7d75460872c0dad
Trentbyu <trentblack@byui.edu>
2022-02-08 14:53:47 -0700

    Update pipe.py

---
+++ game/pipe/pipe.py
@@ -0,0 +1,23 @@
+import raylib 
+import payray 
+
+
+
+class pipe_0():
+    pip_x = []
+    pip_y = []
+    pip_color = []
+    
+    
+    def __init__(self, x, y , width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.hieght = hieght
+        self.color = color
+
+
+
+
+
+
