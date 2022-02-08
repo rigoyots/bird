a45c8edb9075df719fe4b4200cc74c9f8e5f11fe
Trentbyu <trentblack@byui.edu>
2022-02-08 12:48:23 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from raylib import*
+
+import pyray
+
+
+
+
+
+
+
+
+
+def main():
+   
+    
+   
+
+    
+    
+    
+   
+    pyray.init_window(800,600, "spawn")
+    SetWindowState(FLAG_VSYNC_HINT)
+
+    while not WindowShouldClose():
+        
+        
+        
+        pyray.clear_background(BLACK)
+        pyray.end_drawing()
+
+    pyray.close_window()
+
+if __name__ == "__main__":
+    main()
