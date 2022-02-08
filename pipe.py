4329b604c81d0f0e1aaaaf181d3faacf73993a64
Trentbyu <trentblack@byui.edu>
2022-02-08 15:19:21 -0700

    Update pipe.py

--- pipe.py
+++ pipe.py
@@ -40,27 +40,3 @@ class object:
 
 
 
-def main():
-   
-    
-   
-
-    
-    
-    
-   
-    pyray.init_window(800,600, "spawn")
-    SetWindowState(FLAG_VSYNC_HINT)
-
-    while not WindowShouldClose():
-        
-
-        object.pipeloader()
-  
-        pyray.clear_background(BLACK)
-        pyray.end_drawing()
-
-    pyray.close_window()
-
-if __name__ == "__main__":
-    main()
