bb0c847cd5d70c59d681ba189b38a9de23a85ddf
Trentbyu <trentblack@byui.edu>
2022-02-09 16:39:43 -0700

    Update score.py

--- game/score/score.py
+++ game/score/score.py
@@ -10,8 +10,8 @@ def display_score(game_over, score):
     
     
     if not game_over:
-        DrawText(bytes("score: ", encoding='utf8'), 10, 250, int(60), YELLOW)
-        DrawText(bytes(score, encoding='utf8'), 250, 250, int(60), YELLOW)
+        DrawText(bytes("score: ", encoding='utf8'), 10, 250, int(60), GREEN)
+        DrawText(bytes(score, encoding='utf8'), 250, 250, int(60), GREEN)
     
     if game_over:
         DrawText(bytes("game over", encoding='utf8'), 50, 250, int(120), RED)
