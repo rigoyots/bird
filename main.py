21f8ae584c60fb63144b99effead885fbebbac60
Trentbyu <trentblack@byui.edu>
2022-02-08 19:09:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from raylib import *
 from game.pipe.pipe import *
+from game.bird.bird import *
 import pyray
 
 
@@ -23,6 +24,7 @@ def main():
     SetWindowState(FLAG_VSYNC_HINT)
 
     while not WindowShouldClose():
+<<<<<<< HEAD
         
         
         obstacle.make_pipe()
@@ -32,6 +34,21 @@ def main():
         bottom.move_pipes_X()
         bottom.bounds()
       
+=======
+        # Bird
+        Bird.draw_bird()
+        Bird.move_bird()
+        Bird.bird_out_of_bounds()
+    
+       
+       # Pipe
+        object_pipe_1.pipeloader()
+        object_pipe_2.pipeloader()
+        object_pipe_3.pipeloader()
+        objects.move_pipes_X()
+        objects.move_pipes_up_down()
+        objects.bounds()
+>>>>>>> 539b110aafd1531544d8a77723fec1dc396e473e
 
 
         pyray.clear_background(BLACK)
