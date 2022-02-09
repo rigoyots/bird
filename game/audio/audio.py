8a5c3a551f60bddb1f328d6dbee8c875e155617a
Trentbyu <trentblack@byui.edu>
2022-02-09 15:24:20 -0700

    Create audio.py

---
+++ game/audio/audio.py
@@ -0,0 +1,24 @@
+
+from raylib import *
+import pyray
+
+
+def audio():
+    pyray.init_audio_device()
+    
+    flap = pyray.load_sound("flap.wav")
+    
+   
+    
+    pyray.set_sound_volume(flap, 0.5)
+    return flap 
+    
+  
+
+
+def audio_1():
+ 
+    gameover = pyray.load_sound("over.wav")
+    pyray.set_sound_volume(gameover, 0.9)
+
+    return gameover
