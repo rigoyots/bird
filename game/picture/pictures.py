f23444689c5fbd96ab9ab88c2c9df09f54a72057
Trentbyu <trentblack@byui.edu>
2022-02-09 17:01:07 -0700

    Update pictures.py

--- game/picture/pictures.py
+++ game/picture/pictures.py
@@ -6,6 +6,11 @@ import pyray
 
 
 def pipe_1():
+    """
+    loads the image for the bottom pipe
+    """
+    
+    
     pipe_pic = pyray.load_image("downpipe.png")
     pyray.image_resize(pipe_pic, 50, 300)
     p_d_p = pyray.load_texture_from_image(pipe_pic)
@@ -14,6 +19,10 @@ def pipe_1():
 
 
 def pipe_2():
+    """
+   loads the image for the top pipes 
+
+    """
     pipe_pic = pyray.load_image("pipe.png")
     pyray.image_resize(pipe_pic, 50, 300)
     p_p = pyray.load_texture_from_image(pipe_pic)
@@ -21,6 +30,10 @@ def pipe_2():
     return p_p
 
 def bird_pic():
+    """"
+    loads the bird picture
+    """
+    
     flappy = pyray.load_image("flappy3.png")
     pyray.image_resize(flappy, 40, 40)
     p = pyray.load_texture_from_image(flappy)
