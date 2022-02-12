e694a6df9bba4b2d507d92573139b6841e8d94f1
emt0rres <emilee@hatchweb.com>
2022-02-12 11:22:35 -0700

    fixed the problem I think

--- game/Bird/bird.py
+++ game/Bird/bird.py
@@ -21,31 +21,31 @@ class Bird:
         self.y = y
         self.r = r
 
-    def draw_bird(self):
+    def draw_bird():
         """
         draws the bird
         """
-        pyray.draw_circle(int(self.x), int(self.y), self.r, BLACK)
+        pyray.draw_circle(int(bird.x), int(bird.y), bird.r, BLACK)
 
-    def move_bird(self):
+    def move_bird():
         """
         moves the bird
         """
         if IsKeyPressed(KEY_SPACE): 
-            self.y -= 600 * GetFrameTime()
+            bird.y -= 600 * GetFrameTime()
         elif IsKeyDown(KEY_SPACE):
-            self.y -= 600 * GetFrameTime()
+            bird.y -= 600 * GetFrameTime()
         else: #IsKeyUp(KEY_SPACE)
-            self.y += 250 * GetFrameTime()
+            bird.y += 250 * GetFrameTime()
 
 
-    def bird_out_of_bounds(self):
+    def bird_out_of_bounds():
         """
         checks if the bird is out of bounds
         """
-        if self.y <= 0:
-            self.y = 2
-        elif self.y >= 600:
-            self.y = 599
+        if bird.y <= 0:
+            bird.y = 2
+        elif bird.y >= 600:
+            bird.y = 599
 
 bird = Bird(200, 300, 5)
