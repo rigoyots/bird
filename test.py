47a717cf6c5cd817acb536d5f4731b271f2fb03f
Joe <joedevincenzi@gmail.com>
2022-02-11 18:01:29 -0800

    updated test.py
    added 2 tests, will add more.

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-#tests coming soon 
+import pytest
+import pyray
+
+from main import check_collide
+from game.score.score import display_score
+from game.pipe.pipe import obstacle, bottom
+
+#from main
+def test_check_collide():
+    """making sure the collide is returning a bool"""
+    cc = check_collide()
+    assert isinstance(cc, bool) 
+
+#from score
+def test_display_score():
+    game_over = True
+    assert display_score(game_over, 0) != True
+
+
+
+
+
+
+
+
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
