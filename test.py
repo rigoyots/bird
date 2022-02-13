455553b19b6d9092e80fb0bd259d1a2c6f1cce1d
Joe <joedevincenzi@gmail.com>
2022-02-13 14:19:28 -0800

    added bird test to test.py

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ from pyray import Texture
 
 from main import check_collide
 from game.score.score import display_score
-from game.pipe.pipe import obstacle, bottom
-from game.picture.pictures import pictures
+from game.pipe.pipe import Obstacle, Bottom
+from game.picture.pictures import Pictures
+import game.Bird.bird
 
 #from main
 def test_check_collide():
@@ -22,14 +23,33 @@ def test_display_score():
 #from pictures
 def test_pipes():
     """Checking if image overlay for pipes is a texture"""
-    p = pictures()
+    p = Pictures()
     pipe_1 = p.pipe_1()
     assert isinstance(pipe_1, Texture)
     
     pipe_2 = p.pipe_2()
     assert pipe_2 == Texture
 
+def test_movebird(monkeypatch):
+    """
+    Tests: 
+        move_bird()
 
+    Functions:
+        mock_is_key_pressed(x)
+        mock_get_frame_time()
+    """
+    def mock_is_key_pressed(x):
+        return True
+
+    def mock_get_frame_time():
+        return 1
+
+    monkeypatch.setattr("game.Bird.bird.IsKeyPressed", mock_is_key_pressed)
+    monkeypatch.setattr("game.Bird.bird.GetFrameTime", mock_get_frame_time)
+    b = game.Bird.bird.Bird(500, 500, 5)
+    b.move_bird()
+    assert b.y == -100
 
 
 
